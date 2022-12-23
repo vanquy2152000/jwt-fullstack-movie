847f6595986ecdd5412ad62bbd1f7c4d0ba97759
Cao Van Quy <quy.187pm20553@vanlanguni.vn>
2022-12-23 20:52:05 +0700

    fix medialist page

--- src/tmdb/tmdb.endpoints.js
+++ src/tmdb/tmdb.endpoints.js
@@ -2,7 +2,7 @@ import tmdbConfig from "./tmdb.config.js";
 
 const tmdbEndpoints = {
     mediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
-        `${mediaType}/${mediaCategory}`, page
+        `${mediaType}/${mediaCategory}`, { page }
     ),
     mediaDetail: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
         `${mediaType}/${mediaId}`
